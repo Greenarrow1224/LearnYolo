7d9bdc78e496502709dab4f56bf2d1362eb125ad
renxf <greenarrow1949@1633.com>
2024-01-30 11:00:25 +0800

    init project

---
+++ api/test.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+def test():
+    # Create a new YOLO model from scratch
+    model = YOLO('yolov8n.yaml')
+
+    # Load a pretrained YOLO model (recommended for training)
+    model = YOLO('yolov8n.pt')
+
+    # Train the model using the 'coco128.yaml' dataset for 3 epochs
+    results = model.train(data='coco128.yaml', epochs=3)
+
+    # Evaluate the model's performance on the validation set
+    results = model.val()
+
+    # Perform object detection on an image using the model
+    results = model('./Magic.jpg')
+    res = results[0].plot()
+    cv2.imshow("YOLOv8 Detection", res)
+    cv2.waitKey(10)
+    # Export the model to ONNX format
+    # success = model.export(format='onnx')
+if __name__ == '__main__':
+    test()
